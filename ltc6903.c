b4ad8aef98e9587ecbb56495582e96500759c332
Anthony Guerrero <anthony@anthony.moe>
2022-09-27 22:26:01 -0700

    first draft LTC6903 driver

--- ltc6903.c
+++ ltc6903.c
@@ -1,1 +1,12 @@
 #include "ltc6903.h"
+
+void ltc_write(uint16_t oct, uint16_t dac) {
+    uint16_t bitmap = (oct << 12) | (dac << 2);
+    spi_write16_blocking(spi0, &bitmap, 1);
+}
+
+void ltc_set_freq(uint32_t freq) {
+    uint16_t oct = 3.322f * log10(freq / 1039);
+    uint16_t dac = round(2048 - (2078 * pow(2, 10 + oct)) / freq);
+    ltc_write(oct, dac);
+}
--- ltc6903.h
+++ ltc6903.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <math.h>
 #include "hardware/spi.h"
 
 void ltc_set_freq(uint32_t freq);
